44100c0cad4230ea067d9ced725c940ce3b9e20b
azarai <azarai@CowPower.local>
2017-05-10 09:26:49 +0200

    initial revision

---
+++ src/main/java/de/codeboje/springboot/tutorials/actuator/info/InfoActuatorTutorialApplication.java
@@ -0,0 +1,12 @@
+package de.codeboje.springboot.tutorials.actuator.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoActuatorTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoActuatorTutorialApplication.class, args);
+	}
+}
